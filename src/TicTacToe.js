f52b2e5a74af0d081d2444fb830a99ec2ce6988c
leabevi <leabevi@gmail.com>
2022-03-13 13:24:44 -0300

    square created

---
+++ src/TicTacToe.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import "./TicTacToe.css"
+
+const ticTacToe = () => {
+    
+    const HandleClick = ( num) => {
+           alert(num)
+    }
+    const Cell = ({num}) => {
+        return <td onClick={() => HandleClick(num)}>-</td>
+    }
+  return (
+    <div className='container'>
+        <table>
+            <tbody>
+                <tr>
+                    <Cell num={0} />
+                    <Cell num={1} />
+                    <Cell num={2} />
+                </tr>
+                <tr>
+                    <Cell num={3} />
+                    <Cell num={4} />
+                    <Cell num={5} />
+                </tr>
+                <tr>
+                    <Cell num={6} />
+                    <Cell num={7} />
+                    <Cell num={8} />
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default ticTacToe
