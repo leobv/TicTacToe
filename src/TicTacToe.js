b9f54374c9cb402be3062a59a55d7cfd16292d0f
leabevi <leabevi@gmail.com>
2022-03-13 15:31:34 -0300

    Game Complete

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,9 +1,49 @@
 import React, {useState} from 'react'
-import "./TicTacToe.css"
+import './TicTacToe.css'
 
 const TicTacToe = () => {
     const [turn, setTurn]= useState('x');
     const [cells, setCells]= useState(Array(9).fill(''));
+    const [winner, setWinner]= useState()
+
+    const checkForWinner = (squares) => {
+        let combos = {
+            acroos: [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8]
+            ],
+            down: [
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8]
+            ],
+            diagnol:[
+                [0, 4, 8],
+                [2, 4, 6]
+            ]
+        };
+        for (let combo in combos) {
+         combos[combo].forEach((pattern) => {
+             if (squares[pattern[0]] === '' ||
+                 squares[pattern[1]] === '' ||
+                 squares[pattern[2]] === ''
+                 ) {
+                 //do nothing
+             } else if (
+                squares[pattern[0]] === squares[pattern[1]] &&
+                squares[pattern[1]] === squares[pattern[2]]
+             ) {
+                setWinner(squares[pattern[0]])
+                console.log(setWinner)
+             }
+         });   
+        }
+    }
+    const handleRestart = () => {
+        setWinner(null);
+        setCells(Array(9).fill(''))
+    }
     
     const HandleClick = ( num) => {
         if(cells[num] !== '') {
@@ -18,8 +58,9 @@ const TicTacToe = () => {
                 squares[num] = 'o'
                 setTurn('x')
             }
-            setCells(squares)
-            console.log(squares)
+            checkForWinner(squares);
+            setCells(squares);
+            
     }
     const Cell = ({num}) => {
         return <td onClick={() => HandleClick(num)}>{cells[num]}</td>
@@ -46,6 +87,14 @@ const TicTacToe = () => {
                 </tr>
             </tbody>
         </table>
+        {winner && (
+            <div>
+                <p>
+                    {winner} is the winner!
+                </p>
+                <button onClick={() => handleRestart()}>Play Again</button>
+            </div>
+        )}
     </div>
   )
 }
